2a9087a654b42a809e33d42d3ea0759cc85be9a2
zshauiqin <919357403@qq.com>
2016-01-14 08:54:28 +0800

    代码添加到Git库

---
+++ GitHelloworld/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitHelloworld
+{
+    public class Class1
+    {
+        int a = 10;
+    }
+}
